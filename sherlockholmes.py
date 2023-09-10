6abfba6f1948549c73025689585ef70c89f1c208
xxria17 <xxrcy32@gmail.com>
2023-09-10 21:19:20 +0900

    feat: 셜록홈즈 크롤러 수정

--- sherlockholmes.py
+++ sherlockholmes.py
@@ -1,23 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-driver.get("https://sherlock-holmes.co.kr/theme/")
-driver.implicitly_wait(10)
-items = driver.find_elements("class name", "col s3")
-# actions = ActionChains(driver)
+list = []
+link = "https://sherlock-holmes.co.kr/theme/index.php?start="
+driver.get(link + str(len(list)))
 
-for item in items:
-    # actions.move_to_element(item).perform()
+ 
+while len(list) % 12 == 0:  
+    items = driver.find_elements("class name", "col.s3")
+    actions = ActionChains(driver)
+   
+    for item in items:
+        #hover
+        actions.move_to_element(item).perform()
+
+        thumbnail = item.find_element("class name", "img").get_attribute('src')
+        information = item.find_element("class name", "img").find_element("class name", "over").find_element("class name", "inner")
+        title = information.find_element("class name", "tit").text
+        inner_section = information.find_element("class name", "level").find_element("class name", "level_inner").find_element("tag name", "tbody")
+        level_section = inner_section.find_elements("tag name", "tr")
+        genre = level_section[0].find_elements("tag name", "td")[0].text
+        level = len(level_section[0].find_elements("tag name", "td")[1].find_element("class name", "star_box").find_elements("tag name", "i"))
+        location = level_section[1].find_elements("tag name", "td")[1].text
+        
+        print("\t제목 : " + title)
+        print("\t장르 : " + genre)
+        print("\t난이도 : " + str(level))
+        print("\t위치 : " + location)
+        
+        list.append(title)
     
-    # thumbnail = item.find_element(By.CSS_SELECTOR, 'img').get_attribute(['src'])
-    title = item.find_element("class name", 'tit').text
-    genre = item.find_element("css selector", 'td:nth-child(2)').text
-    level = item.find_element("css selector", '.star_box .ico.star').get_attribute('class')
-    location = item.find_element("css selector", 'td[style="padding:5px 0 0; line-height:150%;"]').text
-    
-    print("{\n")
-    print("\t제목 : " + title)
-    print("\t장르 : " + genre)
-    # print("\난이도 : " + str(level))
+    driver.get(link + str(len(list)) + "&")
+    driver.implicitly_wait(2)
