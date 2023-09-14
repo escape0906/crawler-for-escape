87da62d610376948f1b4dda52ab0f337fbb341e7
Minsung Oh <qww1552@naver.com>
2023-09-15 01:31:46 +0900

    feat: data class 적용

--- cubeescape.py
+++ cubeescape.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from model.theme import Theme
 
 driver = webdriver.Chrome()
 driver.get("http://cubeescape.co.kr/")
@@ -9,23 +9,26 @@ stores = driver.find_elements("class name", "circle_div")
 for store in stores:
     location = store.find_element("tag name", "h4").text
     site = store.find_element(By.XPATH, '//a[text()="테마소개"]').get_attribute("href")
-    
+
     print("\매장위치 : " + location)
     driver.get(site)
     driver.implicitly_wait(2)
-    
+
     items = driver.find_elements("class name", "item-main")
 
     for item in items:
-        thumbnail = item.find_element("class name", "portfolio-image").find_element("tag name", "img")
+        thumbnail = item.find_element("class name", "portfolio-image").find_element(
+            "tag name", "img"
+        )
         title = item.find_element("css selector", ".text-center.title").text
-        genre = item.find_element("css selector", ".text-center.title + .text-center").text
+        genre = item.find_element(
+            "css selector", ".text-center.title + .text-center"
+        ).text
         level = len(
             item.find_element("class name", "star").find_elements("tag name", "img")
         )
 
-        print("{\n")
-        print("\t제목 : " + title)
-        print("\t장르 : " + genre)
-        print("\난이도 : " + str(level))
+        data = Theme(title=title, store=location, genre=genre, difficult=level)
+        print(data)
+
     driver.back()
--- nextedition.py
+++ nextedition.py
@@ -1,29 +1,46 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from model.theme import Theme
+from bs4 import BeautifulSoup
+
+
+def extract_text_exclude_child(outerHTML, child_selector):
+    """
+    자식을 제외한 text 추출
+    """
+    soup = BeautifulSoup(outerHTML, "html.parser")
+    inner_elem = soup.select(child_selector)[0].extract()
+    # text_inner_elem = inner_elem.text
+    text_outer_elem = soup.text
+    # print(text_inner_elem)
+    return text_outer_elem.strip()
+
 
 driver = webdriver.Chrome()
 driver.get("https://www.nextedition.co.kr/themes")
 
-items = driver.find_elements("class name", "property-listing-row")
+items = driver.find_elements(By.CLASS_NAME, "property-listing-row")
 for item in items:
-    infomation_card = item.find_element("class name", "listing-content-alt")
-    title = infomation_card.find_element("tag name", "h3")
+    infomation_card = item.find_element(By.CLASS_NAME, "listing-content-alt")
+    title = infomation_card.find_element(By.TAG_NAME, "h3").text
     rating = len(
-        infomation_card.find_element("class name", "rating").find_elements(
-            "tag name", "i"
+        infomation_card.find_element(By.CLASS_NAME, "rating").find_elements(
+            By.TAG_NAME, "i"
         )
     )
-    location = infomation_card.find_element("class name", "listing-location").text
-    detail_box = infomation_card.find_elements(
-        "css selector", ".list-inline .list-inline-item"
+    store = infomation_card.find_element(By.CLASS_NAME, "listing-location").text
+    detail_box = infomation_card.find_elements(By.CSS_SELECTOR, ".list-inline li")
+
+    genre = extract_text_exclude_child(detail_box[0].get_attribute("outerHTML"), "span")
+    recomended_number_of_people = extract_text_exclude_child(
+        detail_box[1].get_attribute("outerHTML"), "span"
     )
-    genre = detail_box[0].text
-    recomended_number_of_people = detail_box[1].text
 
-    print("{\n")
-    print("\t제목 : " + title.text)
-    print("\t별점 : " + str(rating))
-    print("\t위치 : " + location)
-    print("\t장르 : " + genre)
-    print("\t추천 인원수 : " + recomended_number_of_people)
-
-    print("\n}")
+    data = Theme(
+        title=title,
+        difficult=rating,
+        store=store,
+        genre=genre,
+        recomended_number_of_people=recomended_number_of_people,
+    )
+    print(data)
