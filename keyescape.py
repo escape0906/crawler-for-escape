b14e0b1003ef2f0d5ee8e6f9e72713a162c6356e
Minsung Oh <qww1552@naver.com>
2023-09-20 01:13:33 +0900

    feat: keyescape 추상화

--- keyescape.py
+++ keyescape.py
@@ -1,23 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 from model.theme import Theme
+from crawler.ThemeCrawler import ThemeCrawler
 
-response = requests.get("https://www.keyescape.co.kr/web/home.php?go=main")
 
-soup = BeautifulSoup(response.text, "html.parser")
+class KeyescapeCrawler(ThemeCrawler):
+    def get_themes(self):
+        result = []
+        response = requests.get("https://www.keyescape.co.kr/web/home.php?go=main")
 
-items = soup.select(".themeS li>div")
-for item in items:
-    title = item.select_one("a .infoD .title").text
-    infomation_box = item.select("a .imgD .over .in p")
-    genre = infomation_box[0].text.split(":")[-1]
-    play_time = infomation_box[1].text.split(" ")[-1]
-    difficult = 0
-    difficult_imgs = infomation_box[2].select("img")
-    for difficult_img in difficult_imgs:
-        if "star01" in difficult_img["src"]:
-            difficult += 1
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    # 난이도 소수점 버림
-    data = Theme(title=title, store="수기 작성", genre=genre, difficult=difficult)
-    print(data)
+        items = soup.select(".themeS li>div")
+        for item in items:
+            title = item.select_one("a .infoD .title").text
+            infomation_box = item.select("a .imgD .over .in p")
+            genre = infomation_box[0].text.split(":")[-1]
+            play_time = infomation_box[1].text.split(" ")[-1]
+            difficult = 0
+            difficult_imgs = infomation_box[2].select("img")
+            for difficult_img in difficult_imgs:
+                if "star01" in difficult_img["src"]:
+                    difficult += 1
+
+            # 난이도 소수점 버림
+            result.append(
+                Theme(title=title, store="수기 작성", genre=genre, difficult=difficult)
+            )
+
+        return result
