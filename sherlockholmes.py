95c6b8b1606f1d83b9f8bc43f78fa24964c3144e
Minsung Oh <qww1552@naver.com>
2023-09-20 20:18:28 +0900

    feat: sherlock 추상화

--- sherlockholmes.py
+++ sherlockholmes.py
@@ -1,51 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome()
-list = []
-link = "https://sherlock-holmes.co.kr/theme/index.php?start="
-driver.get(link + str(len(list)))
+from crawler.ThemeCrawler import ThemeCrawler
+from model.theme import Theme
 
 
-while len(list) % 12 == 0:
-    items = driver.find_elements("class name", "col.s3")
-    actions = ActionChains(driver)
+class SherlockHolmesCrawler(ThemeCrawler):
+    def get_themes(self) -> list[Theme]:
+        result = []
+        driver = webdriver.Chrome()
+        list = []
+        link = "https://sherlock-holmes.co.kr/theme/index.php?start="
+        driver.get(link + str(len(list)))
 
-    for item in items:
-        # hover
-        actions.move_to_element(item).perform()
+        while len(list) % 12 == 0:
+            items = driver.find_elements("class name", "col.s3")
+            actions = ActionChains(driver)
 
-        thumbnail = item.find_element("class name", "img").get_attribute("src")
-        information = (
-            item.find_element("class name", "img")
-            .find_element("class name", "over")
-            .find_element("class name", "inner")
-        )
-        title = information.find_element("class name", "tit").text
-        inner_section = (
-            information.find_element("class name", "level")
-            .find_element("class name", "level_inner")
-            .find_element("tag name", "tbody")
-        )
-        level_section = inner_section.find_elements("tag name", "tr")
-        genre = level_section[0].find_elements("tag name", "td")[0].text
-        level = len(
-            level_section[0]
-            .find_elements("tag name", "td")[1]
-            .find_element("class name", "star_box")
-            .find_elements("tag name", "i")
-        )
-        store = level_section[1].find_elements("tag name", "td")[1].text
-        if "셜록홈즈" not in store:
-            store = "셜록홈즈 " + store
+            for item in items:
+                # hover
+                actions.move_to_element(item).perform()
 
-        print("\t제목 : " + title)
-        print("\t장르 : " + genre)
-        print("\t난이도 : " + str(level))
-        print("\t위치 : " + store)
+                thumbnail = item.find_element("class name", "img").get_attribute("src")
+                information = (
+                    item.find_element("class name", "img")
+                    .find_element("class name", "over")
+                    .find_element("class name", "inner")
+                )
+                title = information.find_element("class name", "tit").text
+                inner_section = (
+                    information.find_element("class name", "level")
+                    .find_element("class name", "level_inner")
+                    .find_element("tag name", "tbody")
+                )
+                level_section = inner_section.find_elements("tag name", "tr")
+                genre = level_section[0].find_elements("tag name", "td")[0].text
+                level = len(
+                    level_section[0]
+                    .find_elements("tag name", "td")[1]
+                    .find_element("class name", "star_box")
+                    .find_elements("tag name", "i")
+                )
+                store = level_section[1].find_elements("tag name", "td")[1].text
+                if "셜록홈즈" not in store:
+                    store = "셜록홈즈 " + store
 
-        list.append(title)
+                result.append(
+                    Theme(title=title, genre=genre, store=store, difficult=level)
+                )
+                list.append(title)
 
-    driver.get(link + str(len(list)) + "&")
-    driver.implicitly_wait(2)
+            driver.get(link + str(len(list)) + "&")
+            driver.implicitly_wait(2)
+
+        return result
