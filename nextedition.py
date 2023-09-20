67787d2e364917fcf6ad9be3515e086df0bb7b99
Minsung Oh <qww1552@naver.com>
2023-09-20 18:35:45 +0900

    feat: nextedition 추상화

--- nextedition.py
+++ nextedition.py
@@ -2,45 +2,54 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from model.theme import Theme
 from bs4 import BeautifulSoup
+from crawler.ThemeCrawler import ThemeCrawler
 
 
-def extract_text_exclude_child(outerHTML, child_selector):
-    """
-    자식을 제외한 text 추출
-    """
-    soup = BeautifulSoup(outerHTML, "html.parser")
-    inner_elem = soup.select(child_selector)[0].extract()
-    # text_inner_elem = inner_elem.text
-    text_outer_elem = soup.text
-    # print(text_inner_elem)
-    return text_outer_elem.strip()
+class NextEditionCrawler(ThemeCrawler):
+    def get_themes(self) -> list[Theme]:
+        result = []
+        driver = webdriver.Chrome()
+        driver.get("https://www.nextedition.co.kr/themes")
 
+        items = driver.find_elements(By.CLASS_NAME, "property-listing-row")
+        for item in items:
+            infomation_card = item.find_element(By.CLASS_NAME, "listing-content-alt")
+            title = infomation_card.find_element(By.TAG_NAME, "h3").text
+            rating = len(
+                infomation_card.find_element(By.CLASS_NAME, "rating").find_elements(
+                    By.TAG_NAME, "i"
+                )
+            )
+            store = infomation_card.find_element(By.CLASS_NAME, "listing-location").text
+            detail_box = infomation_card.find_elements(
+                By.CSS_SELECTOR, ".list-inline li"
+            )
 
-driver = webdriver.Chrome()
-driver.get("https://www.nextedition.co.kr/themes")
+            genre = self.extract_text_exclude_child(
+                detail_box[0].get_attribute("outerHTML"), "span"
+            )
+            recomended_number_of_people = self.extract_text_exclude_child(
+                detail_box[1].get_attribute("outerHTML"), "span"
+            )
 
-items = driver.find_elements(By.CLASS_NAME, "property-listing-row")
-for item in items:
-    infomation_card = item.find_element(By.CLASS_NAME, "listing-content-alt")
-    title = infomation_card.find_element(By.TAG_NAME, "h3").text
-    rating = len(
-        infomation_card.find_element(By.CLASS_NAME, "rating").find_elements(
-            By.TAG_NAME, "i"
-        )
-    )
-    store = infomation_card.find_element(By.CLASS_NAME, "listing-location").text
-    detail_box = infomation_card.find_elements(By.CSS_SELECTOR, ".list-inline li")
+            data = Theme(
+                title=title,
+                difficult=rating,
+                store=store,
+                genre=genre,
+                recomended_number_of_people=recomended_number_of_people,
+            )
+            result.append(data)
 
-    genre = extract_text_exclude_child(detail_box[0].get_attribute("outerHTML"), "span")
-    recomended_number_of_people = extract_text_exclude_child(
-        detail_box[1].get_attribute("outerHTML"), "span"
-    )
+        return result
 
-    data = Theme(
-        title=title,
-        difficult=rating,
-        store=store,
-        genre=genre,
-        recomended_number_of_people=recomended_number_of_people,
-    )
-    print(data)
+    def extract_text_exclude_child(self, outerHTML, child_selector):
+        """
+        자식을 제외한 text 추출
+        """
+        soup = BeautifulSoup(outerHTML, "html.parser")
+        inner_elem = soup.select(child_selector)[0].extract()
+        # text_inner_elem = inner_elem.text
+        text_outer_elem = soup.text
+        # print(text_inner_elem)
+        return text_outer_elem.strip()
