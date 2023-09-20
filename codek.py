9ee86a2aa2d4c5fedf9f83f3a50528795ee6cbea
Minsung Oh <qww1552@naver.com>
2023-09-20 19:06:01 +0900

    feat: codek 추상화

--- codek.py
+++ codek.py
@@ -3,65 +3,69 @@ from selenium.webdriver.common.by import By
 import re
 import time
 from model.theme import Theme
+from crawler.ThemeCrawler import ThemeCrawler
 
-driver = webdriver.Chrome()
-driver.get("http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S1")
 
-locations = [
-    "http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S1",
-    "http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S2",
-    "http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S3",
-]
+class CodeKCrawler(ThemeCrawler):
+    def get_themes(self) -> list[Theme]:
+        result = []
+        driver = webdriver.Chrome()
+        driver.get("http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S1")
 
-for location in locations:
-    driver.get(location)
-    time.sleep(1)
+        locations = [
+            "http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S1",
+            "http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S2",
+            "http://www.code-k.co.kr/sub/code_sub03.html?R_JIJEM=S3",
+        ]
 
-    store = driver.find_element(By.XPATH, '//*[@id="cont_text"]/ul[2]/span').text.split(
-        " "
-    )[0]
-    print("위치 : " + store)
+        for location in locations:
+            driver.get(location)
+            time.sleep(1)
 
-    themes = driver.find_elements(By.CLASS_NAME, "sspv_list")
-    for theme in themes:
-        title = theme.find_element(By.TAG_NAME, "th").text
-        # print("제목 : " + title)
+            # 매장명
+            store = driver.find_element(
+                By.XPATH, '//*[@id="cont_text"]/ul[2]/span'
+            ).text.split(" ")[0]
 
-        # 상세보기 버튼 클릭
-        detailBtn = theme.find_element(By.XPATH, '//*[@id="btns1"]').click()
+            themes = driver.find_elements(By.CLASS_NAME, "sspv_list")
+            for theme in themes:
+                title = theme.find_element(By.TAG_NAME, "th").text
 
-        description = theme.find_element(
-            By.XPATH, '//*[@id="tab1"]/div[2]/div[1]/ul/li[2]'
-        ).text
-        people_match = re.search(r"인원수 : (\d+-\d+)", description)
-        if people_match:
-            recomended_number_of_people = people_match.group(1)
-        else:
-            recomended_number_of_people = 0
+                # 상세보기 버튼 클릭
+                detailBtn = theme.find_element(By.XPATH, '//*[@id="btns1"]').click()
 
-        genre_match = re.search(r"장르 : (\w+)", description)
-        if genre_match:
-            genre = genre_match.group(1)
-        else:
-            genre = None
+                description = theme.find_element(
+                    By.XPATH, '//*[@id="tab1"]/div[2]/div[1]/ul/li[2]'
+                ).text
+                people_match = re.search(r"인원수 : (\d+-\d+)", description)
+                if people_match:
+                    recomended_number_of_people = people_match.group(1)
+                else:
+                    recomended_number_of_people = 0
 
-        level_match = re.search(r"난이도 : ([★☆]+)", description)
-        if level_match:
-            level = level_match.group(1).count("★")
-        else:
-            level = None
+                genre_match = re.search(r"장르 : (\w+)", description)
+                if genre_match:
+                    genre = genre_match.group(1)
+                else:
+                    genre = None
 
-        # print("추천인원 : "+ recomended_number_of_people)
-        # print("장르 : "+ genre)
-        # print("난이도 : "+ str(level))
+                level_match = re.search(r"난이도 : ([★☆]+)", description)
+                if level_match:
+                    level = level_match.group(1).count("★")
+                else:
+                    level = None
 
-        data = Theme(
-            title=title,
-            store=store,
-            genre=genre,
-            difficult=level,
-            recomended_number_of_people=recomended_number_of_people,
-        )
-        print(data)
-        # 돌아가기 버튼 클릭
-        closeBtn = theme.find_element(By.XPATH, '//*[@id="tab1"]/div[1]').click()
+                data = Theme(
+                    title=title,
+                    store=store,
+                    genre=genre,
+                    difficult=level,
+                    recomended_number_of_people=recomended_number_of_people,
+                )
+                result.append(data)
+                # 돌아가기 버튼 클릭
+                closeBtn = theme.find_element(
+                    By.XPATH, '//*[@id="tab1"]/div[1]'
+                ).click()
+
+        return result
