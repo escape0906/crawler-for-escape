e8a1416c4595c2d46eac8d9afa9bb8f3dee1ffca
Minsung Oh <qww1552@naver.com>
2023-09-20 18:48:01 +0900

    feat: masterkey 추상화

--- masterkey.py
+++ masterkey.py
@@ -1,23 +1,30 @@
 from selenium import webdriver
 from model.theme import Theme
+from crawler.ThemeCrawler import ThemeCrawler
 
-driver = webdriver.Chrome()
-driver.get("https://www.master-key.co.kr/home/theme")
 
-items = driver.find_elements("css selector", ".swiper-slide>a")
+class MasterKeyCrawler(ThemeCrawler):
+    def get_themes(self) -> list[Theme]:
+        result = []
 
-for item in items:
-    title = item.get_attribute("data-title")
-    genre = item.get_attribute("data-type")
-    store = item.get_attribute("data-s2")
-    level = item.get_attribute("data-level")
-    recomended_number_of_people = item.get_attribute("data-people")
+        driver = webdriver.Chrome()
+        driver.get("https://www.master-key.co.kr/home/theme")
 
-    data = Theme(
-        title=title,
-        genre=genre,
-        difficult=level,
-        recomended_number_of_people=recomended_number_of_people,
-        store=store,
-    )
-    print(data)
+        items = driver.find_elements("css selector", ".swiper-slide>a")
+
+        for item in items:
+            title = item.get_attribute("data-title")
+            genre = item.get_attribute("data-type")
+            store = item.get_attribute("data-s2")
+            level = item.get_attribute("data-level")
+            recomended_number_of_people = item.get_attribute("data-people")
+
+            data = Theme(
+                title=title,
+                genre=genre,
+                difficult=level,
+                recomended_number_of_people=recomended_number_of_people,
+                store=store,
+            )
+            result.append(data)
+        return result
