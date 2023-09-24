93b23c6004715aa5e2a3ff020d94b8434e678a06
Minsung Oh <qww1552@naver.com>
2023-09-25 00:36:10 +0900

    feat: 코드케이 썸네일

--- crawler/impl/codek.py
+++ crawler/impl/codek.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 import time
+
 from model.theme import Theme
 from crawler.ThemeCrawler import ThemeCrawler
 
@@ -27,28 +28,37 @@ class CodeKCrawler(ThemeCrawler):
                 By.XPATH, '//*[@id="cont_text"]/ul[2]/span'
             ).text.split(" ")[0]
 
-            themes = driver.find_elements(By.CLASS_NAME, "sspv_list")
+            themes = driver.find_elements(By.CSS_SELECTOR, ".tt_img>ul>li")
             for theme in themes:
-                title = theme.find_element(By.TAG_NAME, "th").text
+                # 썸네일
+                thumbnail = theme.find_element(By.CSS_SELECTOR, "a>img").get_attribute(
+                    "src"
+                )
+                etc = theme.find_element(By.CLASS_NAME, "sspv_list")
+                title = etc.find_element(By.TAG_NAME, "th").text
 
                 # 상세보기 버튼 클릭
-                detailBtn = theme.find_element(By.XPATH, '//*[@id="btns1"]').click()
+                detailBtn = etc.find_element(By.XPATH, '//*[@id="btns1"]').click()
 
-                description = theme.find_element(
+                description = etc.find_element(
                     By.XPATH, '//*[@id="tab1"]/div[2]/div[1]/ul/li[2]'
                 ).text
+
+                # 추천 인원수
                 people_match = re.search(r"인원수 : (\d+-\d+)", description)
                 if people_match:
                     recomended_number_of_people = people_match.group(1)
                 else:
                     recomended_number_of_people = 0
 
+                # 장르
                 genre_match = re.search(r"장르 : (\w+)", description)
                 if genre_match:
                     genre = genre_match.group(1)
                 else:
                     genre = None
 
+                # 난이도
                 level_match = re.search(r"난이도 : ([★☆]+)", description)
                 if level_match:
                     level = level_match.group(1).count("★")
@@ -57,6 +67,7 @@ class CodeKCrawler(ThemeCrawler):
 
                 data = Theme(
                     title=title,
+                    thumbnail=thumbnail,
                     store="코드케이 " + store,
                     genre=genre,
                     difficult=level,
@@ -64,8 +75,6 @@ class CodeKCrawler(ThemeCrawler):
                 )
                 result.append(data)
                 # 돌아가기 버튼 클릭
-                closeBtn = theme.find_element(
-                    By.XPATH, '//*[@id="tab1"]/div[1]'
-                ).click()
+                closeBtn = etc.find_element(By.XPATH, '//*[@id="tab1"]/div[1]').click()
 
         return result
